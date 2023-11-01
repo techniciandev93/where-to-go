7059ea44c5ffd5b9a4acc70c665e43a0b78188c5
anton <a.solovjev@vzv.su>
2023-11-01 11:39:47 +0300

    Добавлен вывод превью фотографий в админке

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from places.models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ('get_preview',)
+    fields = ('image', 'get_preview', 'position')
+
+    def get_preview(self, obj):
+        return format_html(f'<img src="{obj.image.url}" style="max-width:200px; max-height:200px"/>')
 
 
 @admin.register(Place)
@@ -13,4 +20,9 @@ class PlaceAdmin(admin.ModelAdmin):
     ]
 
 
-admin.site.register(Image)
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    readonly_fields = ('get_preview',)
+
+    def get_preview(self, obj):
+        return format_html(f'<img src="{obj.image.url}" style="max-width:200px; max-height:200px"/>')
