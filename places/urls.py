941bf8a990f9b734fdd074edb6156b459b3aea9a
anton <a.solovjev@vzv.su>
2023-11-01 10:57:23 +0300

    Загрузка данных из API

--- places/urls.py
+++ places/urls.py
@@ -3,5 +3,5 @@ from places import views
 
 
 urlpatterns = ([
-    path('<int:place_id>', views.get_place),
+    path('<int:place_id>', views.get_place, name='get_place'),
 ])
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 from places.models import Place
 
 
 def show_main_page(request):
     places_geojson = {
-        "type": "FeatureCollection",
-        "features": []
+        'type': 'FeatureCollection',
+        'features': []
     }
 
     places = Place.objects.all()
     for place in places:
         places_geojson['features'].append(
             {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [place.coordinates_lng, place.coordinates_lat]
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [place.coordinates_lng, place.coordinates_lat]
                 },
-                "properties": {
-                    "title": place.title,
-                    "placeId": place.id,
-                    "detailsUrl": ''
+                'properties': {
+                    'title': place.title,
+                    'placeId': place.id,
+                    'detailsUrl': reverse('get_place', args=(place.id, ))
                 }
             }
         )
