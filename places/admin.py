d9fb70c3255685630d4a7e2283d5b4e706facc40
anton <a.solovjev@vzv.su>
2023-11-01 11:10:50 +0300

    Добавлена загрузка каритонк на странице локации

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from places.models import Place, Image
 
-admin.site.register(Place)
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
+
+
 admin.site.register(Image)
