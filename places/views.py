e28889d9d3fed00410f0d36213af42c2f58061f1
anton <a.solovjev@vzv.su>
2023-11-01 17:01:21 +0300

    added fixes

--- places/views.py
+++ places/views.py
@@ -6,7 +6,7 @@ from places.models import Place
 def get_place(request, place_id):
     obj_place = get_object_or_404(Place, id=place_id)
     place = {'title': obj_place.title,
-             'imgs': [img.image.url for img in obj_place.images.all()],
+             'imgs': [img.image.url for img in obj_place.images.all() if img.image],
              'description_short': obj_place.description_short,
              'description_long': obj_place.description_long,
              'coordinates': {'lng': obj_place.coordinates_lng, 'lat': obj_place.coordinates_lat}}
