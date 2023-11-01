463e0823f06cd1a8454759446157da81f3ed83a4
anton <a.solovjev@vzv.su>
2023-11-01 16:57:17 +0300

    правки в моедлях

--- places/admin.py
+++ places/admin.py
@@ -1,17 +1,13 @@
 from adminsortable2.admin import SortableAdminMixin, SortableInlineAdminMixin
 from django.contrib import admin
-from django.utils.html import format_html
 from places.models import Place, Image
 
 
 class ImageInline(SortableInlineAdminMixin, admin.StackedInline):
     model = Image
-    fields = ('image', 'get_preview')
-    readonly_fields = ('get_preview', )
+    fields = ('image', 'get_preview_image')
+    readonly_fields = ('get_preview_image', )
     extra = 0
-
-    def get_preview(self, obj):
-        return format_html(f'<img src="{obj.image.url}" style="max-width:200px; max-height:200px"/>')
 
 
 @admin.register(Place)
@@ -23,9 +19,7 @@ class PlaceAdmin(admin.ModelAdmin):
 
 @admin.register(Image)
 class ImageAdmin(SortableAdminMixin, admin.ModelAdmin):
-    fields = ('image', 'place', 'get_preview')
+    fields = ('image', 'place', 'get_preview_image')
     raw_id_fields = ('place', )
-    readonly_fields = ('get_preview', )
+    readonly_fields = ('get_preview_image', )
 
-    def get_preview(self, obj):
-        return format_html(f'<img src="{obj.image.url}" style="max-width:200px; max-height:200px"/>')
--- places/models.py
+++ places/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import format_html
 from tinymce.models import HTMLField
 
 
@@ -8,6 +9,10 @@ class Place(models.Model):
     description_long = HTMLField(verbose_name='Полное описание')
     coordinates_lng = models.FloatField(verbose_name='Долгота')
     coordinates_lat = models.FloatField(verbose_name='Широта')
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
 
     def __str__(self):
         return self.title
@@ -20,6 +25,15 @@ class Image(models.Model):
 
     class Meta:
         ordering = ['position']
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
 
     def __str__(self):
         return f'{self.position} {self.place.title}'
+
+    def get_preview_image(self):
+        if self.image:
+            return format_html(f'<img src="{self.image.url}" style="max-width:200px; max-height:200px"/>')
+        return 'Здесь будет изображение'
+
+    get_preview_image.short_description = 'Вывод изображения'
