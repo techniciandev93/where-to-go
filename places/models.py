9167740482d9414161f3ce4ae51360bb34480e0d
anton <a.solovjev@vzv.su>
2023-11-02 16:47:57 +0300

    Правки в моделях

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=50, verbose_name='Заголовок')
-    description_short = models.TextField(max_length=300, verbose_name='Короткое описание')
+    title = models.CharField(max_length=50, verbose_name='Заголовок', unique=True)
+    description_short = HTMLField(max_length=300, verbose_name='Короткое описание')
     description_long = HTMLField(verbose_name='Полное описание')
     coordinates_lng = models.FloatField(verbose_name='Долгота')
     coordinates_lat = models.FloatField(verbose_name='Широта')
